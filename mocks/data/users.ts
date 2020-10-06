d35a5ace4fb623e0cbe719afdeecb81454d33e3b
kjkandrea <kjkandrea@gmail.com>
2020-10-06 12:33:25 +0900

    eslint 관련 환경 구성

---
+++ mocks/data/users.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 1,
+    name: 'andrea'
+  },
+  {
+    id: 2,
+    name: 'haebogoyang'
+  },
+  {
+    id: 3,
+    name: 'mummu'
+  }
+]
--- snippets/SayHello.ts
+++ snippets/SayHello.ts
@@ -1,6 +1,6 @@
-import ProfileData from '../mocks/data/profile'
-import { Profile } from '../types/data/index'
+import ProfileData from "../mocks/data/profile";
+import { Profile } from "../types/data/index";
 
-const data: Profile = ProfileData
+const data: Profile = ProfileData;
 
-console.log(`Hello, ${data.name}!`)
+console.log(`Hello, ${data.name}!`);
