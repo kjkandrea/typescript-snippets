81ea954874cdef9c24bda8d06227a6d3375168c4
kimjongkeun <jongkeun@godo.co.kr>
2020-10-06 16:57:38 +0900

    KeyReplace : add

---
+++ snippets/object/replace/KeyReplace.ts
@@ -0,0 +1,22 @@
+import UserData from "../../../mocks/data/user";
+import { User } from "../../../types/data/index";
+
+const data: User = UserData;
+const newKeys = { id: "index", name: "nickname" };
+const user = replaceKeyName(data, newKeys);
+
+function replaceKeyName(
+  obj: { [key: string]: any },
+  newKeys: { [key: string]: any }
+) {
+  const keyValues = Object.keys(obj).map((key) => {
+    const newKey = newKeys[key] || key;
+    return {
+      [newKey]: obj[key],
+    };
+  });
+
+  return Object.assign({}, ...keyValues);
+}
+
+console.log(user);
