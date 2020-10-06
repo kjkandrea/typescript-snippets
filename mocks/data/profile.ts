00e8b030ed7870f0e913ad6d8ddcc83b7bdc67dc
kimjongkeun <jongkeun@godo.co.kr>
2020-10-06 12:00:39 +0900

    init

---
+++ mocks/data/profile.ts
@@ -0,0 +1,4 @@
+export default {
+  name: "andrea",
+  birthDay: "1992-01-07T00:00"
+}
---
+++ snippets/SayHello.ts
@@ -0,0 +1,5 @@
+import ProfileData from '../mocks/data/profile'
+
+const data = ProfileData
+
+console.log(`Hello, ${data.name}!`)
